01e453a25b8e6fc9816db0e413eb61425d6896a0
ameera <ameeraabed92@gmail.com>
2021-03-16 19:21:41 +0200

    under work

---
+++ login.js
@@ -0,0 +1,21 @@
+let username = document.getElementById("usernaame-input");
+let goBtn = document.getElementById("go");
+let password = document.getElementById("password");
+let form = document.getElementById("form");
+let emptyfieldWarning = document.getElementById("emptyfieldWarning");
+
+goBtn.onclick = function() {
+
+    emptyfieldWarning.classList.remove("warning");
+    if (username.value && password.value) {
+        let userValue = username.value;
+        localStorage.setItem("user", userValue);
+        location.href = "index.html";
+    } else {
+
+        emptyfieldWarning.classList.add("warning");
+
+        emptyfieldWarning.innerHTML = "please enter your username or password"
+
+    }
+}
