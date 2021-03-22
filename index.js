1b05fa80e9f9fb10966d7bd11d5a8c7451175278
ameera <ameeraabed92@gmail.com>
2021-03-22 18:46:52 +0200

    new update

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 let submitbtn = document.getElementById("submit-btn");
+let newsfeed = document.getElementById("newsfeed");
 
-
-// tweet button when we click on it
-//submitbtn.onclick = function {
-
-//}
-// creat tweet function that creat tweet html element and add to page
+//tweet button when we click on it
+submitbtn.onclick = function() {
+        creatTweet();
+    }
+    // creat tweet function that creat tweet html element and add to page
 function creatTweet() {
     let comment = document.createComment("creat new tweet");
     let arrDiv = [];
-    for (let i = 0; i <= 6; i++) {
+    let addClassArr = ["newTweet", "tname", "tweetItSelf", "tweetBtn", "tBtn", "tBtn"]
+    arrDiv.push(document.createElement("div"));
+    newsfeed.appendChild(arrDiv[0]);
+    arrDiv[0].classList.add(addClassArr[0]);
+    for (let i = 1; i <= 5; i++) {
         arrDiv.push(document.createElement("div"));
+        if (i <= 3) {
+            arrDiv[0].appendChild(arrDiv[i]);
+            arrDiv[i].classList.add(addClassArr[i]);
+        } else {
+            arrDiv[3].appendChild(arrDiv[i]);
+            arrDiv[i].classList.add(addClassArr[i]);
+        }
     }
-    console.log(arrDiv);
+
 
 }
-creatTweet();
